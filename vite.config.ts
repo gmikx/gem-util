116478b53989016968be35ccbff2cbcc57cf84d5
Mikołaj Giza <93527772+gmikx@users.noreply.github.com>
2026-04-14 20:12:37 +0200

    split the build into smaller chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
     },
   },
   server: {
